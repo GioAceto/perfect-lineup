54c00da0e5a2b28deb42fdae9a72784355810cf5
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-15 20:54:43 -0400

    add sumsSalaries to validateLineup function so it returns false if the total salaries exceed 45000

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const validateLineup = () => {
+const validateLineup = (lineup) => {
+  const sumsSalaries = lineup.reduce((total, player) => (total + player.salary), 0)
 
+  if (sumsSalaries < 45000) {
+    return true
+  }
+
+  return false
 }
 
 module.exports = validateLineup
