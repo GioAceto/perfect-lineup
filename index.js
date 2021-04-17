7c1c9a8f65074be16355d732fe7d57aa0bd743b0
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-17 07:35:12 -0400

    change totalSalary function to be outside of validateLineup function

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const validateLineup = (lineup) => {
-  const sumsSalaries = lineup.reduce((total, player) => (total + player.salary), 0)
-
-  if (sumsSalaries < 45000) {
-    return true
-  }
-
-  return false
+const totalSalary = (lineup) => {
+  return lineup.reduce((total, player) => {
+    return total + player.salary
+  }, 0)
 }
+
+const validateSalary = (lineup) => totalSalary(lineup) < 45000
+
+
+
+const validateLineup = (lineup) => {
+  return validateSalary(lineup)
+}
+
+
 
 module.exports = validateLineup
