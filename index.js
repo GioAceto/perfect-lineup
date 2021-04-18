ba358c602d1a14175577a9a59c1fb8605567942a
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-18 10:39:20 -0400

    add separate functions to return each position count

--- index.js
+++ index.js
@@ -6,13 +6,33 @@ const totalSalary = (lineup) => {
 
 const validateSalary = (lineup) => totalSalary(lineup) < 45000
 
-getPositionCounts = (lineup) => {
-  return lineup.reduce((total, player) => {
-    total[player.position] = total[player.position] === undefined ? 1 : total[player.position] + 1
+const getOF = lineup.filter(function (e) {
+  return e.position === 'OF'
+})
 
-    return total
-  }, {})
-}
+const getP = lineup.filter(function (e) {
+  return e.position === 'P'
+})
+
+const getC = lineup.filter(function (e) {
+  return e.position === 'C'
+})
+
+const get1B = lineup.filter(function (e) {
+  return e.position === '1B'
+})
+
+const get2B = lineup.filter(function (e) {
+  return e.position === '2B'
+})
+
+const get3B = lineup.filter(function (e) {
+  return e.position === '3B'
+})
+
+const getSS = lineup.filter(function (e) {
+  return e.position === 'SS'
+})
 
 
 
