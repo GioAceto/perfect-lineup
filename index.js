5e01ccab387868c22dab46ca7b6a6dfad95411c0
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-04-18 11:11:16 -0400

    refactored module exports in positions js file

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const getOF = require('./positions')
+
+
 const totalSalary = (lineup) => {
   return lineup.reduce((total, player) => {
     return total + player.salary
@@ -5,34 +8,6 @@ const totalSalary = (lineup) => {
 }
 
 const validateSalary = (lineup) => totalSalary(lineup) < 45000
-
-const getOF = (lineup) => lineup.filter(function (e) {
-  return e.position === 'OF'
-})
-
-const getP = (lineup) => lineup.filter(function (e) {
-  return e.position === 'P'
-})
-
-const getC = (lineup) => lineup.filter(function (e) {
-  return e.position === 'C'
-})
-
-const get1B = (lineup) => lineup.filter(function (e) {
-  return e.position === '1B'
-})
-
-const get2B = (lineup) => lineup.filter(function (e) {
-  return e.position === '2B'
-})
-
-const get3B = (lineup) => lineup.filter(function (e) {
-  return e.position === '3B'
-})
-
-const getSS = (lineup) => lineup.filter(function (e) {
-  return e.position === 'SS'
-})
 
 
 const validateLineup = (lineup) => {
